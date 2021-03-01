dde23ecedd9b1fd87078760435cfed5d3469afbe
Emma <emmandersson166@gmail.com>
2021-03-01 16:48:45 +0100

    added form

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -4,12 +4,24 @@ import styles from "../css/Sidebar.module.css";
 const sidebar = () => {
   return (
     <div className={styles.sidebar}>
-       <div className={styles.heading}>
-         <p>Filter</p>
-         <p>Clear</p>
-       </div>
+      <div className={styles.heading}>
+        <p>Search Filters</p>
+
+      </div>
+      <form>
+        <label htmlFor="make">Make</label>
+        <input type="text" placeholder="Toyota" />
+
+        <label htmlFor="model">Model</label>
+        <input type="text" placeholder="Camry" />
+
+        <label htmlFor="year">Year</label>
+        <input type="number" placeholder="1972"/>
+
+        <label htmlFor="make">Price</label>
+        <input type="range" min="1" max="10000000" value="1" className={styles.priceRange} />
+      </form>
     </div>
-   
   );
 };
 
