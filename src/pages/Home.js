b5d4babf5d5a5fac28db6b2f2880eb7a7154f8a5
Emma <emmandersson166@gmail.com>
2021-03-01 15:57:17 +0100

    added sidebar component in home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
+import Sidebar from "../components/Sidebar";
 import style from "../css/Home.module.css";
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      <div id={style.search_field_container}>Search Field</div>
+      <div id={style.search_field_container}>
+        <Sidebar />
+      </div>
       <div id={style.product_listing_container}>Product Listing</div>
     </div>
   );
