c884a3cc14bb39be740f1a4b62c6e56d5bac6a15
Johnny Lay <johnny.lay.contact@gmail.com>
2021-03-01 18:15:22 +0100

    Added basic information for product card + updated css for product card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,7 +1,31 @@
 import style from "../css/ProductCard.module.css";
 
 const ProductCard = () => {
-  return <div className={style.product_card}>Card</div>;
+  return (
+    <div className={style.product_card}>
+      <div className={style.img_wrapper}>
+        <img src="" alt="product-image" />
+      </div>
+      <p className={style.card_information}>
+        <span>Make:</span> <br />
+        <span>Model:</span> <br />
+        <span>Year:</span>
+        <br />
+        <span>VIN:</span>
+        <br />
+        <span>City:</span>
+        <br />
+        <span>Price:</span> <br />
+        <span>Distance: 39,333 miles</span>
+        <br />
+      </p>
+      <p className={style.card_description}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere,
+        delectus eaque quasi debitis perspiciatis ratione nobis ipsum ab!
+        Ratione, facilis.
+      </p>
+    </div>
+  );
 };
 
 export default ProductCard;
