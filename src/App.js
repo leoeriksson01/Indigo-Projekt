a5cc65356df086b6e101c173820a5198f9c98f9f
Leo <leoeriksson01@gmail.com>
2021-03-01 17:04:11 +0100

    Added a footer with social media icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import CarContextProvider from "./components/contexts/CarContext";
@@ -9,6 +10,7 @@ export default function App() {
       <div className="App">
         <Router>
           <Navbar />
+          <Footer/>
           <Home />
           <Route exact path="/" />
           <Route exact path="/About-us" />
