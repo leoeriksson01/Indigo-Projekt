0d4df97acfdcc50a747b7d221ea8ce22f6a56513
Johnny Lay <johnny.lay.contact@gmail.com>
2021-03-01 15:40:05 +0100

    Added parentheses

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import Navbar from "./components/Navbar";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import CarContextProvider from './components/contexts/CarContext';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import CarContextProvider from "./components/contexts/CarContext";
 import Home from "./pages/Home";
 export default function App() {
-    return (
-        <CarContextProvider>
-            <div className="App">
-      <Home />
-     <Router>
-        <Navbar />
+  return (
+    <CarContextProvider>
+      <div className="App">
+        <Home />
+        <Router>
+          <Navbar />
           <Route exact path="/" />
           <Route exact path="/About-us" />
-      </Router>
-            </div>
-        </CarContextProvider>
+        </Router>
+      </div>
+    </CarContextProvider>
+  );
 }
