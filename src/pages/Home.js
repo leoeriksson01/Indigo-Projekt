b03770769974ca3869eb480760df2b19e0294c2b
Johnny Lay <johnny.lay.contact@gmail.com>
2021-03-01 15:14:11 +0100

    Created containers for: search-field, product listing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
-import "../css/Home.module.css";
+import style from "../css/Home.module.css";
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      
+      <div id={style.search_field_container}>Search Field</div>
+      <div id={style.product_listing_container}>Product Listing</div>
     </div>
   );
 };
