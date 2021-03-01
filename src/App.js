a763e56a35295af1400c334ef9f73954df47d412
Leo <leoeriksson01@gmail.com>
2021-03-01 11:51:14 +0100

    Created a Navbar component
    Created a bootstrap navbar and changed the background color to the intended color

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Navbar from "./components/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+     <Navbar/>
+
+  </div>;
 }
 
 export default App;
