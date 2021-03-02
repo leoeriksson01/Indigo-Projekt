1ba6f34e4bd8381cd3de39cd39a83f42b6a6e263
Emma <emmandersson166@gmail.com>
2021-03-02 19:24:04 +0100

    updated app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CarContextProvider from "./components/contexts/CarContext";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Car from "./pages/Car";
