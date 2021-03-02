45f6cb9b5b27d907e8d8b7dc37e7ad1f21896400
Johnny Lay <johnny.lay.contact@gmail.com>
2021-03-02 18:09:29 +0100

    Created addToCart function & shoppingCart (useState) for context

--- src/components/contexts/CarContext.js
+++ src/components/contexts/CarContext.js
@@ -1,33 +1,47 @@
-import { createContext, useState, useEffect } from 'react';
-import defaultCars from '../../json/cars.json';
+import { createContext, useState, useEffect } from "react";
+import defaultCars from "../../json/cars.json";
 
 export const CarContext = createContext();
 
 export default function CarContextProvider({ children }) {
-    const [cars, setCars] = useState([]);
+  const [cars, setCars] = useState([]);
+  const [shoppingCart, setShoppingCart] = useState([]);
 
-    useEffect(() => {
-        setCars(defaultCars);
-    }, []);
+  useEffect(() => {
+    setCars(defaultCars);
+  }, []);
 
-    // Remove car whose vin property matches the one being sent in
-    function remove(carToRemove = {}) {
-        setCars(p => p.filter(car => car.vin !== carToRemove.vin));
+  // Adds product to shopping cart
+  function addToCart(car) {
+    const newShoppingList = [...shoppingCart, car];
+
+    // Checks if shoppingCart already contains car/product
+    if (shoppingCart.some((product) => product.vin === car.vin)) {
+      return;
+    } else {
+      // if shoppingCart does not already contains car/product, new product/car will be pushed into the shoppingCart
+      setShoppingCart(newShoppingList);
     }
+  }
 
-    // Return array of matches
-    function find(key = 'vin', value = '') {
-        return cars.filter(car => car[key] === value);
-    }
+  // Remove car whose vin property matches the one being sent in
+  function remove(carToRemove = {}) {
+    setCars((p) => p.filter((car) => car.vin !== carToRemove.vin));
+  }
 
-    // Return single car object on match, null if empty
-    function findOne(key = 'vin', value ='') {
-        return cars.find(car => car[key] === value) ?? null;
-    }
+  // Return array of matches
+  function find(key = "vin", value = "") {
+    return cars.filter((car) => car[key] === value);
+  }
 
-    return (
-        <CarContext.Provider value={{ cars, find, findOne, remove }}>
-            {children}
-        </CarContext.Provider>
-    );
+  // Return single car object on match, null if empty
+  function findOne(key = "vin", value = "") {
+    return cars.find((car) => car[key] === value) ?? null;
+  }
+
+  return (
+    <CarContext.Provider value={{ cars, find, findOne, remove }}>
+      {children}
+    </CarContext.Provider>
+  );
 }
