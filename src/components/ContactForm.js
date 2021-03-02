d16ec95bd4d4e90da767892b3b471a1f9e0f75d4
Lovisa Nordström <lovisa.nordstroms@gmail.com>
2021-03-02 15:08:46 +0100

    Created contact form component file + contact form css file

---
+++ src/components/ContactForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "../components/ContactForm.css";
+
+function ContactForm (props) {
+    return (
+        <form id={style.formContainer}>
+            <p>test</p>
+        </form>
+    )
+}
+
+export default ContactForm
