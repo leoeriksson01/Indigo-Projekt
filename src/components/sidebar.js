33fd642bf466c862c097e31cf272d61f2563f695
Emma <emmandersson166@gmail.com>
2021-03-02 12:25:52 +0100

    made the sidebar responsive + toggle

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../css/Sidebar.module.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFilter } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilter } from "@fortawesome/free-solid-svg-icons";
 
-const sidebar = () => {
+const Sidebar = () => {
+  //Usestates
+  const [show, setShow] = useState(false);
+
+  //Functions
+  const handleShow = () => {
+    setShow(!show);
+  };
+
+  // Add class active
+  const activeClass = show ? styles.active : "";
+
   return (
     <div className={styles.sidebar}>
-      <div className={styles.heading}>
-        <div><p>Filter</p></div>
-       <div><FontAwesomeIcon className={styles.filter} icon={faFilter} /></div>
+      <div className={styles.heading} onClick={handleShow}>
+        <div className={styles.filter}>
+          <p>Filter</p>
+          <FontAwesomeIcon icon={faFilter} />
+        </div>
       </div>
-      <form>
+
+      <form className={`${activeClass}`}>
         <label htmlFor="make">Make</label>
         <input type="text" placeholder="Toyota" />
 
@@ -18,13 +32,17 @@ const sidebar = () => {
         <input type="text" placeholder="Camry" />
 
         <label htmlFor="year">Year</label>
-        <input type="number" placeholder="1972"/>
+        <input type="number" placeholder="1972" />
 
         <label htmlFor="make">Price</label>
-        <input type="range" min="1" max="10000000" value="1" className={styles.priceRange} />
+        <input type="range" min="1" max="10000000" />
+
+        <label htmlFor="make">Miles</label>
+        <input type="range" min="1" max="10000000" />
+  
       </form>
     </div>
   );
 };
 
-export default sidebar;
+export default Sidebar;
