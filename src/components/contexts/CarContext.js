ba0a22d852407d7077fe8dd9bed5a03bf9608b34
Philip <philip_angelin@hotmail.com>
2021-03-03 00:12:38 +0100

    Refactoring

--- src/components/contexts/CarContext.js
+++ src/components/contexts/CarContext.js
@@ -15,30 +15,28 @@ export default function CarContextProvider({ children }) {
 
   // Adds product to shopping cart
   function addToCart(car) {
-    const newShoppingList = [...shoppingCart, car];
-
     // Checks if shoppingCart already contains car/product
-    if (shoppingCart.some((product) => product.vin === car.vin)) {
+    if (shoppingCart.some(product => product.vin === car.vin)) {
       return;
     } else {
       // if shoppingCart does not already contains car/product, new product/car will be pushed into the shoppingCart
-      setShoppingCart(newShoppingList);
+      setShoppingCart(p => [ ...p, car ]);
     }
   }
 
   // Remove car whose vin property matches the one being sent in
   function remove(carToRemove = {}) {
-    setCars((p) => p.filter((car) => car.vin !== carToRemove.vin));
+    setCars(p => p.filter(car => car.vin !== carToRemove.vin));
   }
 
   // Return array of matches
   function find(key = "vin", value = "") {
-    return cars.filter((car) => car[key] === value);
+    return cars.filter(car => car[key] === value);
   }
 
   // Return single car object on match, null if empty
   function findOne(key = "vin", value = "") {
-    return cars.find((car) => car[key] === value) ?? null;
+    return cars.find(car => car[key] === value) ?? null;
   }
 
   return (
