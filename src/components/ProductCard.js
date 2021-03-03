0cd0ed9eb17ba63d63483ea1cee59664e5e1dda9
Johnny Lay <johnny.lay.contact@gmail.com>
2021-03-03 12:09:52 +0100

    Created wrapper for card information

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -27,25 +27,31 @@ const ProductCard = () => {
             alt="product"
           />
         </div>
-        <p className={style.card_information}>
-          <span>Make: {car.make}</span> <br />
-          <span>Model: {car.model} </span> <br />
-          <span>Year: {car.year}</span>
-          <br />
-          <span>VIN: {car.vin}</span>
-          <br />
-          <span>City: {car.city}</span>
-          <br />
-          <span>Price: {car.price}</span> <br />
-          <span>Distance: {car.miles} miles</span>
-          <br />
-        </p>
-        <p className={style.card_description}>
-          <span className={style.car_description_title}>Car description:</span>
-          <br />
-          <span>{car.descShort}</span>
-          <br />
-        </p>
+
+        <div className={style.card_information_wrapper}>
+          <p className={style.card_information}>
+            <span>Make: {car.make}</span> <br />
+            <span>Model: {car.model} </span> <br />
+            <span>Year: {car.year}</span>
+            <br />
+            <span>VIN: {car.vin}</span>
+            <br />
+            <span>City: {car.city}</span>
+            <br />
+            <span>Price: {car.price}</span> <br />
+            <span>Distance: {car.miles} miles</span>
+            <br />
+          </p>
+          <p className={style.card_description}>
+            <span className={style.car_description_title}>
+              Car description:
+            </span>
+            <br />
+            <span>{car.descShort}</span>
+            <br />
+          </p>
+        </div>
+        {/* /.card_information_wrapper */}
         <div className={style.button_wrapper}>
           <button
             onClick={() => handleButtonAdd(car)}
