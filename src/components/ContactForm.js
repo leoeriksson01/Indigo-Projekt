6c2815e869d893cfb51512a2cace60738ac5f18a
Lovisa Nordström <lovisa.nordstroms@gmail.com>
2021-03-03 10:41:50 +0100

    Added heading + 2 input fields for contactForm

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,12 +1,39 @@
 import React from "react";
 import style from "../components/ContactForm.css";
 
+
 function ContactForm (props) {
     return (
-        <form id={style.formContainer}>
-            <p>test</p>
-        </form>
+        <div id={style.formContainer}>
+            <form id={style.form}> 
+                <h1>Get in touch! Send us a message below:</h1>
+                <div id={style.formInputs}>
+                    <label htmlFor="name" 
+                    id={style.formLabel}>
+                        <input 
+                        type="text" 
+                        name="name" 
+                        id={style.formInput}
+                        placeholder="Enter your name"
+                        />
+                    </label>
+                </div>
+                <div id={style.formInputs}>
+                    <label htmlFor="email" 
+                    id={style.formLabel}>
+                        <input 
+                        type="email" 
+                        name="email" 
+                        id={style.formInput}
+                        placeholder="Enter your email"
+                        />
+                    </label>
+                </div>
+            </form>
+        </div>
     )
 }
+
+
 
 export default ContactForm
