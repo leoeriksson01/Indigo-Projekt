c5ce54254572b9aeb80cb9c7cd4c39956a0b2f90
Emma <emmandersson166@gmail.com>
2021-03-03 14:02:11 +0100

    added form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,7 +1,22 @@
 const SearchForm = () => {
-  return ( 
-  <div></div>
-   );
-}
- 
+  return (
+    <form>
+      <label htmlFor="make">Make</label>
+      <input type="text" placeholder="Toyota" />
+
+      <label htmlFor="model">Model</label>
+      <input type="text" placeholder="Camry" />
+
+      <label htmlFor="year">Year</label>
+      <input type="number" placeholder="1972" />
+
+      <label htmlFor="make">Price</label>
+      <input type="range" min="1" max="10000000" />
+
+      <label htmlFor="make">Miles</label>
+      <input type="range" min="1" max="10000000" />
+    </form>
+  );
+};
+
 export default SearchForm;
