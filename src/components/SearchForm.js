3a3557b091de9a4de81e0cd0f2398b2b69b140dd
Emma <emmandersson166@gmail.com>
2021-03-03 15:33:56 +0100

    created select dropdown with makes from context

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,21 +1,57 @@
+import React, { useState, useContext } from "react";
+import { CarContext } from "../components/contexts/CarContext";
+import styles from "../css/SearchForm.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilter } from "@fortawesome/free-solid-svg-icons";
+
 const SearchForm = () => {
+  //States
+  const [show, setShow] = useState(false);
+
+  //Context
+  const { cars } = useContext(CarContext);
+
+  //Functions
+  const handleShow = () => {
+    setShow(!show);
+    console.log(show);
+  };
+
+  // Add class active
+  const activeClass = show ? styles.active : "";
+
   return (
-    <form>
-      <label htmlFor="make">Make</label>
-      <input type="text" placeholder="Toyota" />
+    <div>
+      <div className={styles.heading} onClick={handleShow}>
+        <div className={styles.filter}>
+          <p>Filter</p>
+          <FontAwesomeIcon className={styles.filter_icon} icon={faFilter} />
+        </div>
+      </div>
 
-      <label htmlFor="model">Model</label>
-      <input type="text" placeholder="Camry" />
+      <form className={`${activeClass}`}>
+        <label htmlFor="make">Make</label>
+        <select>
+          {Array.from(
+            new Set(cars.map((obj) => obj.make))
+          ).map((make) => {
+            return <option value={make}>{make}</option>;
+          })}
+        </select>
 
-      <label htmlFor="year">Year</label>
-      <input type="number" placeholder="1972" />
+        <label htmlFor="model">Model</label>
+        <input type="text" placeholder="Camry" />
 
-      <label htmlFor="make">Price</label>
-      <input type="range" min="1" max="10000000" />
+        <label htmlFor="year">Year</label>
+        <input type="number" placeholder="1972" />
 
-      <label htmlFor="make">Miles</label>
-      <input type="range" min="1" max="10000000" />
-    </form>
+        <label htmlFor="make">Price</label>
+        <input type="range" min="1" max="10000000" />
+
+        <label htmlFor="make">Miles</label>
+        <input type="range" min="1" max="10000000" />
+      </form>
+    </div>
   );
 };
 
