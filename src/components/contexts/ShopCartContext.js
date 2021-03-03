219828931cf894a27ab73cebb235f11bb66511e5
Johnny Lay <johnny.lay.contact@gmail.com>
2021-03-03 07:24:52 +0100

    Created shoppingCart(useState) & addToCart function

--- src/components/contexts/ShopCartContext.js
+++ src/components/contexts/ShopCartContext.js
@@ -3,10 +3,22 @@ import { createContext, useState, useEffect } from "react";
 export const ShopCartContext = createContext();
 
 export default function ShopCartContextProvider({ children }) {
+  
+  const [shoppingCart, setShoppingCart] = useState([]);
 
+  // Adds product to shopping cart
+  function addToCart(car) {
+    const newShoppingList = [...shoppingCart, car];
+
+    // Checks if shoppingCart already contains car/product
+    if (shoppingCart.some((product) => product.vin === car.vin)) {
+      return;
+    } else {
+      // if shoppingCart does not already contains car/product, new product/car will be pushed into the shoppingCart
+      setShoppingCart(newShoppingList);
+    }
+  }
   return (
-    <ShopCartContext.Provider value={{ }}>
-      {children}
-    </ShopCartContext.Provider>
+    <ShopCartContext.Provider value={{}}>{children}</ShopCartContext.Provider>
   );
 }
