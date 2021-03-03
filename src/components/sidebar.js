ab0d36c259d6d85e2353edfc7365f33a531485fa
Emma <emmandersson166@gmail.com>
2021-03-03 14:03:08 +0100

    removed form

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -23,24 +23,6 @@ const Sidebar = () => {
           <FontAwesomeIcon className={styles.filter_icon} icon={faFilter} />
         </div>
       </div>
-
-      <form className={`${activeClass}`}>
-        <label htmlFor="make">Make</label>
-        <input type="text" placeholder="Toyota" />
-
-        <label htmlFor="model">Model</label>
-        <input type="text" placeholder="Camry" />
-
-        <label htmlFor="year">Year</label>
-        <input type="number" placeholder="1972" />
-
-        <label htmlFor="make">Price</label>
-        <input type="range" min="1" max="10000000" />
-
-        <label htmlFor="make">Miles</label>
-        <input type="range" min="1" max="10000000" />
-  
-      </form>
     </div>
   );
 };
