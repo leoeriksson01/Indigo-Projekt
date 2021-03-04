4acf44796bf74a7cf8ddbcfde043539f8b78fc12
Johnny Lay <johnny.lay.contact@gmail.com>
2021-03-04 13:59:28 +0100

    Cleaned out duplicate codes from context // Johnny & Leo

--- src/components/contexts/ShopCartContext.js
+++ src/components/contexts/ShopCartContext.js
@@ -24,7 +24,6 @@ export default function ShopCartContextProvider({ children }) {
 
   // Adds product to shopping cart
   function addToCart(car) {
- 
     // Checks if shoppingCart already contains car/product
     if (shoppingCart.some((product) => product.vin === car.vin)) {
       return;
@@ -35,49 +34,14 @@ export default function ShopCartContextProvider({ children }) {
       // sets total price of shoppingCart
       setTotalPrice(totalPrice + car.price);
     }
-    
   }
+
   //Removes product from shoppingCart
   function removeProduct(car) {
     const newList = shoppingCart.filter((product) => product.vin !== car.vin);
     setShoppingCart(newList);
     setTotalPrice(totalPrice - car.price);
   }
-
-  function removeProduct(car) {
-    const newList = shoppingCart.filter(product => product.vin !== car.vin)
-    setShoppingCart(newList)
-    setTotalPrice(totalPrice-car.price)
-  }
-
-/*   const [products, setProducts] = useState(JSON.parse(localStorage.getItem('products')))
-
-  const [isProductsChanged, setProductsChanged] = useState(false)
-
-  useEffect(() => {
-    // If isStudentsChanged === true, we save our 'students' list to local storage
-    if(isProductsChanged){
-      localStorage.setItem('products', JSON.stringify(products))
-    }
-
-    // Also, set isStudentsChanged back to false.
-    setProductsChanged(false)
-    
-    // useEffect hook runs only if we isStudentsChanged variable is set to true.
-  }, [isProductsChanged])
-
-  // This function is used in "Programming.js" component
-  const removeProduct = (productToRemove) =>{
-    const updatedProducts = products.filter(product => product.vin !== productToRemove.vin)
-    setProducts(updatedProducts)
-
-    // SetStudentsChanged is equal to true, since we want the new list to be saved in localStorage
-    // This runs useEffect hook
-    setProductsChanged(true)
-    
-  } */
-
-
 
   return (
     <ShopCartContext.Provider
