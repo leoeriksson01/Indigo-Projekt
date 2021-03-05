5fcd46c99ed881d029b182f48f81369834998fee
Lovisa Nordström <lovisa.nordstroms@gmail.com>
2021-03-05 12:01:02 +0100

    Created folder for Carousel components, imported to Home

---
+++ src/components/carouselComponents/CarouselSlider.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function CarouselSlider(props) {
+    return (
+      <div>Test</div>
+    )
+  }
+  export default CarouselSlider;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import Sidebar from "../components/sidebar.js";
 import style from "../css/Home.module.css";
 import ProductListing from "../components/ProductListing";
+import Carousel from "../components/carouselComponents/CarouselSlider";
 
 const Home = () => {
   return (
     <div className={style.wrapper}>
+      <div id={style.carouselContainer}>
+        <Carousel/>
+      </div>
       <div id={style.search_field_container}>
         <Sidebar />
       </div>
