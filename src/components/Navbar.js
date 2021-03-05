06d7e175a89668a59d10508d8bd60562937973b2
Leo <leoeriksson01@gmail.com>
2021-03-05 14:46:58 +0100

    Made adjustments to the navbar design and made it more responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { ShopCartContext } from "./contexts/ShopCartContext";
 import { ReactComponent as CloseMenu } from "../assets/x.svg";
+import { ReactComponent as Profile } from "../assets/profile.svg";
 import { ReactComponent as MenuIcon } from "../assets/menu.svg";
 import { ReactComponent as Cart } from "../assets/cart.svg";
 import { useContext, useRef } from "react";
@@ -11,14 +12,19 @@ import ShoppingCart from "../components/ShoppingCart";
 
 const Header = () => {
   function toggleCart() {
-    console.log(cartList.current);
-
     if (cartList.current) {
       cartList.current.classList.toggle(style.hidden);
     }
   }
 
+  function toggleProfile() {
+    if (profileList.current) {
+      profileList.current.classList.toggle(style.hidden);
+    }
+  }
+
   const cartList = useRef();
+  const profileList = useRef();
 
   const { totalPrice } = useContext(ShopCartContext);
   const [click, setClick] = useState(false);
@@ -30,14 +36,16 @@ const Header = () => {
         <div className={style.logo}>
           <img src={logo} />
         </div>
+        <div className={style.bilgagnat}>Bilgagnat</div>
+
         <ul className={`${style.nav_options} ${click ? style.active : ""}`}>
           <li className={style.option} onClick={closeMobileMenu}>
-            <NavLink className={style.a} exact to="/">
+            <NavLink exact to="/">
               Home
             </NavLink>
           </li>
           <li className={style.option} onClick={closeMobileMenu}>
-            <NavLink className={style.a} exact to="/about">
+            <NavLink exact to="/about">
               About
             </NavLink>
           </li>
@@ -50,10 +58,17 @@ const Header = () => {
           <ShoppingCart />
           <div className={style.totalprice}>
             Total price: ${totalPrice}
-            <NavLink className={style.a} exact to="/checkout">
+            <NavLink exact to="/checkout">
               To Checkout
             </NavLink>
           </div>
+        </div>
+      </div>
+
+      <div className={style.profile_container}>
+        <Profile className={style.profile} onClick={toggleProfile} />
+        <div className={style.profileList} ref={profileList}>
+          {" "}
         </div>
       </div>
 
