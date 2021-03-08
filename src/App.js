1b3f78a9785bc613e381c7e47b40accbbdef1299
Philip <philip_angelin@hotmail.com>
2021-03-08 15:54:45 +0100

    Bugfix

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import CheckoutPage from './components/CheckoutPage';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CarContextProvider from "./components/contexts/CarContext";
 import Home from "./pages/Home";
 import Car from "./pages/Car";
-import AboutPage from "./pages/AboutPage";
 import CheckoutConfirm from "./components/CheckoutConfirm";
 export default function App() {
   return (
@@ -17,9 +16,6 @@ export default function App() {
           <Switch>
             <Route exact path="/">
               <Home />
-            </Route>
-            <Route exact path="/about">
-              <AboutPage />
             </Route>
             <Route exact path="/car/:vin">
               <Car />
