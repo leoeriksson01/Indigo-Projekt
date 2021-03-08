1bfc71451789a5ecb5aaf341e0df5709e2c092eb
Lovisa Nordström <lovisa.nordstroms@gmail.com>
2021-03-08 16:16:02 +0100

    Added prevSlide function

--- src/components/carouselComponents/CarouselSlider.js
+++ src/components/carouselComponents/CarouselSlider.js
@@ -7,19 +7,17 @@ const [current, setCurrent] = useState (0)
 const length = slides.length
 
 const nextSlide = () => {
-  setCurrent(current === length - 1 ? 0 : current +1)
-}
+  setCurrent(current === length - 1 ? 0 : current + 1);
+};
 
-console.log(current);
-
-if(!Array.isArray(slides) || slides.length <= 0) {
-  return null;
-}
+const prevSlide = () => {
+  setCurrent(current === 0 ? length - 1 : current - 1);
+};
 
 
   return (
     <div id={style.slider}>
-      <div id={style.circleLeft}>
+      <div id={style.circleLeft} onClick={prevSlide}>
         <i id={style.arrowLeft}></i>
       </div>
       <div id={style.circleRight} onClick={nextSlide}>
