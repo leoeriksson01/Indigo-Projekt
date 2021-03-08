2c29593c5246538f2c462da7f613852ac249141f
Lovisa Nordström <lovisa.nordstroms@gmail.com>
2021-03-08 15:41:21 +0100

    Added nextSlide function

--- src/components/carouselComponents/CarouselSlider.js
+++ src/components/carouselComponents/CarouselSlider.js
@@ -6,13 +6,23 @@ const CarouselSlider = ( slides ) => {
 const [current, setCurrent] = useState (0)
 const length = slides.length
 
+const nextSlide = () => {
+  setCurrent(current === length - 1 ? 0 : current +1)
+}
+
+console.log(current);
+
+if(!Array.isArray(slides) || slides.length <= 0) {
+  return null;
+}
+
 
   return (
     <div id={style.slider}>
       <div id={style.circleLeft}>
         <i id={style.arrowLeft}></i>
       </div>
-      <div id={style.circleRight}>
+      <div id={style.circleRight} onClick={nextSlide}>
         <i id={style.arrowRight}></i>
       </div>
       {SliderData.map((slide, index) => {
