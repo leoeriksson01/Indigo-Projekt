9bd478e1c489a250064e86ed2cd9cd966aea9b21
Lovisa Nordström <lovisa.nordstroms@gmail.com>
2021-03-08 14:40:03 +0100

    Created arrow buttons for carousel

--- src/components/carouselComponents/CarouselSlider.js
+++ src/components/carouselComponents/CarouselSlider.js
@@ -1,15 +1,23 @@
 import React, {useState} from 'react';
 import SliderData from "./PlaceholderImages";
+import style from "../../css/Carousel.module.css";
 
 const CarouselSlider = () => {
 const [current, setCurrent] = useState (0)
 //const length = slides.length
 
+
   return (
     <div>
+      <div id={style.circle}>
+        <i id={style.arrowLeft}></i>
+      </div>
       {SliderData.map((slide, index) => {
         return <img src={slide.image} alt="Placeholder car" />
       })}
+      <div id={style.circle}>
+        <i id={style.arrowRight}></i>
+      </div>
     </div>
   );
 };
