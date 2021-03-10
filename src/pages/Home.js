da439f2ccf774cc0785607c5beee12ccfcad8e33
Johnny Lay <johnny.lay.contact@gmail.com>
2021-03-10 10:22:59 +0100

    Updated home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,20 +2,18 @@ import style from "../css/Home.module.css";
 import ProductListing from "../components/ProductListing";
 import Form from "../components/SearchForm";
 
-
 const Home = () => {
   return (
-    <div className={style.wrapper}>
-      <div id={style.search_field_container}>
-      <Form/>
-      </div>
-      <div id={style.product_listing_container}>
-        <ProductListing />
-      </div>
-    </div>
+    <p>Home</p>
+    // <div className={style.wrapper}>
+    //   <div id={style.search_field_container}>
+    //   <Form/>
+    //   </div>
+    //   <div id={style.product_listing_container}>
+    //     <ProductListing />
+    //   </div>
+    // </div>
   );
 };
 
 export default Home;
-
-
