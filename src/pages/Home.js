d4dfc4b6136e34de6133b3b945e1bdc43be7660b
Johnny Lay <johnny.lay.contact@gmail.com>
2021-03-10 10:23:50 +0100

    Added productlisting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,15 +4,15 @@ import Form from "../components/SearchForm";
 
 const Home = () => {
   return (
-    <p>Home</p>
-    // <div className={style.wrapper}>
-    //   <div id={style.search_field_container}>
-    //   <Form/>
-    //   </div>
-    //   <div id={style.product_listing_container}>
-    //     <ProductListing />
-    //   </div>
-    // </div>
+    // <p>Home</p>
+    <div className={style.wrapper}>
+      <div id={style.search_field_container}>
+      <Form/>
+      </div>
+      <div id={style.product_listing_container}>
+        <ProductListing />
+      </div>
+    </div>
   );
 };
 
