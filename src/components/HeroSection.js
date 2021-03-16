78f7070ec86866cf9174ec6f483ffe0e5d77f74f
Lovisa Nordström <lovisa.nordstroms@gmail.com>
2021-03-16 15:13:33 +0100

    Added hero image

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,10 +1,12 @@
 import React from "react";
 import style from "../css/Hero.module.css";
+import heroImage from "../assets/hero-image.jpg";
 
 function HeroSection(props) {
     return (
       <div id={style.heroContainer}>
           Test
+          {<img src={heroImage} id={style.heroImage} alt="Image of a landscape and road."/>}
       </div>
     )
   }
