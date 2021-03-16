66e2c17ae4275d39ebc58d19fdcb7cf24b1c9871
Lovisa Nordström <lovisa.nordstroms@gmail.com>
2021-03-16 21:31:14 +0100

    Imported carousel into hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,13 +1,17 @@
 import React from "react";
 import style from "../css/Hero.module.css";
 import heroImage from "../assets/hero-image.jpg";
+import Carousel from "../components/carouselComponents/CarouselSlider";
+import array from "../json/cars.json";
 
 function HeroSection(props) {
     return (
       <div id={style.heroContainer}>
           {<img src={heroImage} id={style.heroImage} alt="Image of a landscape and road."/>}
           <div id={style.bannerOverlay}></div>
-          <div id={style.carouselArea}>Placeholder</div>
+          <div id={style.carouselArea}>
+            <Carousel cars={array} />
+          </div>
       </div>
     )
   }
