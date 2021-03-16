e2f5a317f76491f5eff6499e57c052d1a218b4c8
Johnny Lay <johnny.lay.contact@gmail.com>
2021-03-16 10:02:11 +0100

    Added profile menu for dropdown // Leo & Johnny

--- src/components/ProfileMenu.js
+++ src/components/ProfileMenu.js
@@ -1,11 +1,46 @@
 import style from "../css/ProfileMenu.module.css";
+import { NavLink } from "react-router-dom";
 
 const ProfileMenu = () => {
+	const loggedInMenu = (
+		<div className={style.logged_in_menu}>
+			<div className={style.order_link_wrapper}>
+				<NavLink exact to="#" className={style.a}>
+					My orders
+				</NavLink>
+			</div>
+			<hr className={style.hr} />
+			<div className={style.button_logout_wrapper}>
+				<button className={style.button_logout}>Log out</button>
+			</div>
+		</div>
+	);
+
+	const loggedOutMenu = (
+		<div className={style.logged_out_menu}>
+			<div className={style.button_login_wrapper}>
+				<button className={style.button_login}>Log in</button>
+			</div>
+			<div className={style.registration_wrapper}>
+				<p className={style.registration_text}>
+					<span className={style.registration_text}>Not a member?</span>
+					<NavLink className={style.a} exact to="#">
+						Register now
+					</NavLink>
+				</p>
+			</div>
+			<hr className={style.hr} />
+			<div className={style.order_link_wrapper}>
+				<NavLink exact to="#" className={style.a}>
+					My orders
+				</NavLink>
+			</div>
+		</div>
+	);
+
 	return (
 		<div className={style.profile_menu_wrapper}>
-			<div className={style.profile_menu_content}>
-				This is the Profile Menu
-			</div>
+			<div className={style.profile_menu_content}>{ loggedInMenu }</div>
 		</div>
 	);
 };
