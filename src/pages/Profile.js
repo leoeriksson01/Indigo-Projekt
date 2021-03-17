2b561253fbf0856937a17c22c7a81ad9c315c93f
Leo <leoeriksson01@gmail.com>
2021-03-17 12:24:57 +0100

    Added sidebar and styling to profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,9 +3,23 @@ import style from "../css/Profile.module.css";
 const Profile = () => {
 	return (
 		<div className={style.profileContainer}>
-			<div className={style.profileSidebar}> </div>
 			<div className={style.profileContent}>
-				<div className={style.greetingText}>Hello Name</div>
+				<div className={style.greetingText}>
+					{" "}
+					<h2>Hello Name </h2>
+					<p>On "My Profile" you can view your Order History and Messages</p>
+				</div>
+				<div className={style.messages}>
+					<h3 className={style.messagesHeader}> Messages </h3>
+					<div className={style.messageList}></div>
+				</div>
+			</div>
+			<div className={style.profileSidebar}>
+				<div className={style.headerText}> My Profile </div>
+				<ul className={style.links}>
+					<li> My Orders </li>
+					<li> Help & Contact </li>
+				</ul>
 			</div>
 		</div>
 	);
