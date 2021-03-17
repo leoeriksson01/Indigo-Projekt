3b497cc5b071a57609df11db9c2dc4b2794e2b30
Leo <leoeriksson01@gmail.com>
2021-03-17 13:45:36 +0100

    Added content and responsiveness to profile

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -3,12 +3,19 @@ import style from "../css/Message.module.css";
 const Messages = () => {
 	return (
 		<div className={style.messageContainer}>
-			<div className={style.status}>
-				Status: <span className={style.pending}> Pending </span>
+			<span className={style.nameStatus}>
+				<div className={style.name}>Kristian Tyrann</div>
+				<div className={style.status}>
+					Status: <span className={style.pending}> Pending </span>
+				</div>
+			</span>
+
+			<div className={style.email}> Email: Tyrannen07@gmail.com </div>
+			<div className={style.message}>
+				{" "}
+				Message: Jeg er morderen Johannes og jeg er årsagen til Stockholms
+				massakre
 			</div>
-			<div className={style.name}> Name </div>
-			<div className={style.email}> Email </div>
-			<div className={style.message}> Message </div>
 		</div>
 	);
 };
