37cd20cd10b18eea4f73db9797746ce7b4390455
Johnny Lay <johnny.lay.contact@gmail.com>
2021-03-17 20:11:55 +0100

    Styling profileinformation component

--- src/components/ProfileInformation.js
+++ src/components/ProfileInformation.js
@@ -3,49 +3,50 @@ import style from "../css/ProfileInformation.module.css";
 const ProfileInformation = () => {
 	return (
 		<div className={style.profile_information_content}>
-			<div className={style.personal_information}>
+			<div className={style.profile_information_header}>
 				<h2 className={style.h2}>Personal information</h2>
-				<div className={style.text_information}>
-					<p className={style.p}>
-						Name: <br />
-						<span className={style.span}>Admin Admin</span>
-					</p>
-					<p className={style.p}>
-						Address: <br />
-						<span className={style.span}>Admin12345</span>
-					</p>
-					<p className={style.p}>
-						County: <br />
-						<span className={style.span}>Skåne</span>
-					</p>
-					<p className={style.p}>
-						Zip Code: <br />
-						<span className={style.span}>200 00</span>
-					</p>
-
-					<p className={style.p}>
-						Email: <br />
-						<span className={style.span}>admin@bilgagnat.com</span>
-					</p>
-					<p className={style.p}>
-						Password: <br />
-						<span className={style.span}>**********</span>
-					</p>
-				</div>
 			</div>
-
-			<div className={style.billing_information}>
-				<h2 className={style.h2}>Billing information</h2>
-				<p>
-					Credit Card: <span>Master Card</span>
+			<div className={style.personal_information}>
+				<p className={style.p}>
+					Name: <br />
+					<span className={style.span}>Admin Admin</span>
 				</p>
-				<p>
-					Card number: <span>**** ****</span>
+				<p className={style.p}>
+					Address: <br />
+					<span className={style.span}>Admin12345</span>
 				</p>
-				<p>
-					VVC: <span>***</span>
+				<p className={style.p}>
+					County: <br />
+					<span className={style.span}>Skåne</span>
+				</p>
+				<p className={style.p}>
+					Zip Code: <br />
+					<span className={style.span}>200 00</span>
 				</p>
 			</div>
+			{/* ./personal_information */}
+			<hr />
+			<div className={style.login_information}>
+				<h3 className={style.h2}>Login information</h3>
+				<p className={style.p}>
+					Email: <br />
+					<span className={style.span}>admin@bilgagnat.com</span>
+				</p>
+				<p className={style.p}>
+					Password: <br />
+					<span className={style.span}>**********</span>
+				</p>
+			</div>
+			{/* /.login_information */}
+			<hr />
+			<div className={style.payment_method}>
+				<h3 className={style.h2}>Payment method</h3>
+				<p className={style.p}>
+					Email: <br />
+					<span className={style.span}>admin@bilgagnat.com</span>
+				</p>
+			</div>
+			{/* /.payment_method */}
 		</div>
 	);
 };
