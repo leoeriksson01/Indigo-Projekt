93a0a859905d7589e459e347a8f1ba792eaac98f
Johnny Lay <johnny.lay.contact@gmail.com>
2021-03-17 18:06:27 +0100

    Added NavLink for sidebar

--- src/components/ProfileSidebar.js
+++ src/components/ProfileSidebar.js
@@ -1,4 +1,5 @@
 import style from "../css/ProfileSidebar.module.css";
+import { NavLink } from "react-router-dom";
 
 const ProfileSidebar = () => {
 	return (
@@ -7,8 +8,16 @@ const ProfileSidebar = () => {
 				<h2 className={style.header_text}>My Profile</h2>
 			</div>
 			<ul className={style.links}>
-				<li>My Orders</li>
-				<li>Help & Contact</li>
+				<li>
+					<NavLink className={style.a} exact to="#">
+						My Orders
+					</NavLink>
+				</li>
+				<li>
+					<NavLink className={style.a} exact to="#">
+						Help & Contact
+					</NavLink>
+				</li>
 			</ul>
 		</div>
 	);
