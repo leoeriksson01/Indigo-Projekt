b631dd6629532458525d284bb148124e218ea15a
Lovisa Nordström <lovisa.nordstroms@gmail.com>
2021-03-17 21:23:56 +0100

    some styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,15 +6,17 @@ import Hero from "../components/HeroSection";
 const Home = () => {
   return (
     // <p>Home</p>
-  <div>
-    <Hero/>
+  <div id={style.home}>
+    <div id={style.heroComponent}>
+      <Hero/>
+    </div>
     <div className={style.wrapper}>
       <div id={style.search_field_container}>
         <Form/>
       </div>
       <div id={style.product_listing_container}>
         <ProductListing />
-    </div>
+      </div>
     </div>
   </div>  
   );
