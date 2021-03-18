e4d05f582c017454592cbc19e5d01782c51b872f
Lovisa Nordström <lovisa.nordstroms@gmail.com>
2021-03-18 08:56:23 +0100

    Created SignUp.js + css file

---
+++ src/components/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from '../css/SignUp.module.css'
+
+const SignUp = () => {
+  return (
+    <div id={style.SignUpWrapper}>
+        test
+    </div>
+  );
+}
+
+export default SignUp;
