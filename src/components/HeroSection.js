f35dfaf03281c1490a8a3bcc4f72a8d4d722397a
Lovisa Nordström <lovisa.nordstroms@gmail.com>
2021-03-18 08:23:14 +0100

    more styling

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -8,12 +8,12 @@ function HeroSection(props) {
     return (
       <div id={style.heroContainer}>
           {<img src={heroImage} id={style.heroImage} alt="Image of a landscape and road."/>}
+          <h1 id={style.campaignHeader}>Cars on <span id={style.saleColor}>sale!</span></h1>
           <div id={style.bannerOverlay}>
             <div id={style.carouselArea}>
                 <Carousel cars={array} />
             </div>
           </div>
-
       </div>
     )
   }
