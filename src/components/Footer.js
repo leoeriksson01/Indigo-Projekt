0ef18eef53f9cd5a0ede9157d3c9725772819f59
Emma <emmandersson166@gmail.com>
2021-03-21 22:14:17 +0100

    refactored footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,40 +9,30 @@ import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className={style.footer}>
-      <h2 className={style.headerText}> Our Socials </h2>
-      <div className={style.contactContainer}>
-        <div className={style.container}>
-          <div className={style.logo_container}>
-            <NavLink className={style.a} exact to="/">
-              <Twitter className={style.logo} />
-            </NavLink>
-          </div>
-
-          <div className={style.logo_container}>
-            <NavLink className={style.a} exact to="#">
-              <Instagram className={style.logo} />
-            </NavLink>
-          </div>
-
-          <div className={style.logo_container}>
-            <NavLink className={style.a} exact to="#">
-              <Facebook className={style.logo} />
-            </NavLink>
-          </div>
-        </div>
-        <div className={style.contact}>
-          <NavLink className={style.a} exact to="#">
-            <Phone className={style.contactLogo} />
-            +070 531 20 90
-          </NavLink>
-          <NavLink className={style.a} exact to="#">
-            <Mail className={style.contactLogo} />
-            Bilgagnat@contact.com
-          </NavLink>
-        </div>
+    <footer>
+      <div className={style.contact}>
+        <NavLink className={style.a} exact to="#">
+          hello@bilgagnat.com
+        </NavLink>
+        <NavLink className={style.a} exact to="#">
+          +46 70 531 20 90
+        </NavLink>
       </div>
-    </div>
+
+      <div className={style.logo_container}>
+        <NavLink className={style.a} exact to="/">
+          <Twitter className={style.logo} />
+        </NavLink>
+
+        <NavLink className={style.a} exact to="#">
+          <Instagram className={style.logo} />
+        </NavLink>
+
+        <NavLink className={style.a} exact to="#">
+          <Facebook className={style.logo} />
+        </NavLink>
+      </div>
+    </footer>
   );
 };
 
