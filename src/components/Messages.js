3a88d81d9b5da12ba104d92f76644b3012ed05ce
Johnny Lay <johnny.lay.contact@gmail.com>
2021-03-24 12:15:01 +0100

    Fixed profile page - Profile information & Message box
     Emma, Leo,  Johnny, Philip

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,11 +1,15 @@
 import style from "../css/Messages.module.css";
+import { UserContext } from "./contexts/UserContext";
+import { useContext } from "react";
 
 const Messages = () => {
-	return (
-		<div className={style.message_container}>
+	const { getMessages } = useContext(UserContext);
+
+	return getMessages().map((msg, i) => (
+		<div className={style.message_container} key={i}>
 			<div className={style.name_status_wrapper}>
 				<div className={style.name_wrapper}>
-					<h3 className={style.name}>User</h3>
+					<h3 className={style.name}>{msg.name}</h3>
 				</div>
 				<div className={style.status}>
 					<p className={style.pending}>Pending</p>
@@ -14,13 +18,13 @@ const Messages = () => {
 			{/* /.name_status_wrapper */}
 
 			<p className={style.email}>
-				Email: <span className={style.span}>user@contact.com</span>
+				Email: <span className={style.span}>{msg.email}</span>
 			</p>
 			<p className={style.message}>
-				Message: <span className={style.span}>This is my message!</span>
+				Message: <span className={style.span}>{msg.message}</span>
 			</p>
 		</div>
-	);
+	));
 };
 
 export default Messages;
