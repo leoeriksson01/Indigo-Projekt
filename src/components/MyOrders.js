dc0752cf1830a5145c3b3bb67da66d74a73c14cc
Philip <philip_angelin@hotmail.com>
2021-03-25 11:50:08 +0100

    Fixed MyOrders so it uses the new function

--- src/components/MyOrders.js
+++ src/components/MyOrders.js
@@ -1,35 +1,37 @@
 import Order from "./Order";
 import style from "../css/MyOrders.module.css";
 import ProfileSidebar from "../components/ProfileSidebar";
+import { useContext } from "react";
+import { UserContext } from "./contexts/UserContext";
 
 const MyOrders = () => {
-	const orders = JSON.parse(localStorage.getItem("orders"));
+	const { getOrders } = useContext(UserContext);
 
 	return (
-    <div className={style.myOrdersContainer}>
-      <div className={style.ordersContent}>
-        <div className={style.orderGreetingText}>
-          <h2>My orders</h2>
-          <p>Here on "My Orders" you can view your Order History</p>
-        </div>
-        <hr className={style.orderHr} />
+		<div className={style.myOrdersContainer}>
+			<div className={style.ordersContent}>
+				<div className={style.orderGreetingText}>
+					<h2>My orders</h2>
+					<p>Here on "My Orders" you can view your Order History</p>
+				</div>
+				<hr className={style.orderHr} />
 				<div className={style.orderInfoWrap}>
-					{orders.map((order, i) => {
+					{getOrders().map((order, i) => {
 						return (
 							<div className={style.orderContainer} key={i}>
 								<div className={style.orderWrapper}>
-									<Order order={order} />
+									<Order order={order.items} />
 								</div>
 							</div>
 						);
 					})}
 				</div>
-      </div>
-      <div className={style.orderSidebar}>
-        <ProfileSidebar />
-      </div>
-    </div>
-  );
+			</div>
+			<div className={style.orderSidebar}>
+				<ProfileSidebar />
+			</div>
+		</div>
+	);
 };
 
 export default MyOrders;
